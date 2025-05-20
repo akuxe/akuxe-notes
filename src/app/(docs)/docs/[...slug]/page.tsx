b4f922eefd65fb0dc49a61ef9fce1e3d019a6243
Arun <akmnrucode@gmail.com>
2025-05-20 20:26:41 +0530

    refactor(docs/layout): change padding to margin

--- src/app/(docs)/docs/[...slug]/page.tsx
+++ src/app/(docs)/docs/[...slug]/page.tsx
@@ -74,7 +74,7 @@ export default async function DocPage({
 
   return (
     <>
-      <div className='grid min-h-dvh grid-cols-1 lg:grid-cols-[minmax(0,1fr)_var(--container-2xs)]'>
+      <div className='grid grid-cols-1 lg:grid-cols-[minmax(0,1fr)_var(--container-2xs)]'>
         {/* Main content */}
         <main className='px-4 py-10 sm:px-6'>
           <div className='md:hidden'>
--- src/app/(docs)/layout.tsx
+++ src/app/(docs)/layout.tsx
@@ -11,7 +11,7 @@ export default async function Layout({
     <div className='mx-auto max-w-360'>
       <DocsSidebarMobile />
 
-      <div className='grid min-h-dvh grid-cols-1 grid-rows-1 justify-center pt-[calc(64px+56px)] md:grid-cols-[var(--container-2xs)_minmax(0,calc(768px+288px))] md:pt-16'>
+      <div className='mt-[calc(64px+56px)] grid min-h-dvh grid-cols-1 grid-rows-1 justify-center md:mt-16 md:grid-cols-[var(--container-2xs)_minmax(0,calc(768px+288px))]'>
         {/* Sidebar */}
         <aside className='col-start-1 row-span-full max-md:hidden'>
           <div className='sticky top-16 max-h-[calc(100dvh-64px)] overflow-y-auto px-6 py-10 [scrollbar-width:thin]'>
